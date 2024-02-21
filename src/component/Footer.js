b46fe1a8560f6ff44289e53e2dd1cdf0d2b81d11
herold park <heroldpark@gmail.com>
2024-02-22 00:28:23 +0900

    2024-02-22 react-study 최초 commit

---
+++ src/component/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer(props) {
+    return (
+        <div>
+            <footer>
+                <h2>푸터입니다.</h2>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/component/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Main(props) {
+    return (
+        <div>
+            <main>
+                <h2>메인입니다.</h2>
+            </main>
+        </div>
+    );
+}
+
+export default Main;
